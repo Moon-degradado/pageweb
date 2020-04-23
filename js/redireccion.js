a44d704a4345ec8972fdba6752e3b3d44874709f
Jahaziel Esau Luna Hernandez <jahaziel_e_luna_hdez@hotmail.com>
2020-04-22 23:20:59 -0500

    change js

--- js/redireccion.js
+++ js/redireccion.js
@@ -2,8 +2,6 @@ window.onload = function() {
     document.getElementById('que_es').onclick = function () {
         window.location = "Que_es.html";
     }
-}
-window.onload = function() {
     document.getElementById('Beneficios').onclick = function () {
         window.location = "Beneficios.html";
     }
